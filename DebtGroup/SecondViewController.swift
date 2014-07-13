689a18853f3d7b0e7c05331c6d21faed3bfd7f89
Ryan Batchelder <rb688@nau.edu>
2014-07-12 19:45:31 -0700

    Initial Commit with Xcode Project Template

---
+++ DebtGroup/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DebtGroup
+//
+//  Created by Ryan Batchelder on 7/12/14.
+//  Copyright (c) 2014 Ryan Batchelder. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
