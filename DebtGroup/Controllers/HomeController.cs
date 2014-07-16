bc3b4cf90b9e538f25de1fc759563cf468f22ac4
Ryan Batchelder <rb688@nau.edu>
2014-07-16 08:19:06 -0700

    Working on getting OAuth system working.

--- DebtGroup/Controllers/HomeController.cs
+++ DebtGroup/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Google.GData.Spreadsheets;
+using Google.GData.Client;
 
 namespace DebtGroup.Controllers
 {
@@ -28,5 +30,43 @@ namespace DebtGroup.Controllers
 
             return View();
         }
+
+		public ActionResult DoAuth()
+		{
+			string ClientId = Environment.GetEnvironmentVariable("Google_Client_Id");
+			string ClientSecret = Environment.GetEnvironmentVariable ("Google_Client_Secret");
+			string Scope = "https://spreadsheets.google.com/feeds https://docs.google.com/feeds";
+			string RedirectUri = "http://debtgroup.azurewebsites.net/oauth2callback";
+			OAuth2Parameters parameters = new OAuth2Parameters();
+			parameters.ClientId = ClientId;
+			parameters.ClientSecret = ClientSecret;
+			parameters.Scope = Scope;
+			parameters.RedirectUri = RedirectUri;
+			string OAuthUrl = OAuthUtil.CreateOAuth2AuthorizationUrl (parameters);
+			HttpContext.Application ["GoogleAuthParams"] = parameters;
+			return Redirect (OAuthUrl);
+		}
+
+		public ActionResult oauth2callback()
+		{
+			return View ();
+		}
+
+		public ActionResult SheetApp(string token)
+		{
+			OAuth2Parameters parameters = HttpContext.Application ["GoogleAuthParams"] as OAuth2Parameters;
+			parameters.AccessCode = token;
+			OAuthUtil.GetAccessToken (parameters);
+			GOAuth2RequestFactory requestFactory = new GOAuth2RequestFactory (null, "MySpreadsheetIntegration-v1", parameters);
+			SpreadsheetsService service = new SpreadsheetsService ("MySpreadsheetIntegration-v1");
+			service.RequestFactory = requestFactory;
+			SpreadsheetQuery query = new SpreadsheetQuery ();
+			SpreadsheetFeed feed = service.Query (query);
+			foreach (SpreadsheetEntry entry in feed.Entries) 
+			{
+				ViewBag.Sheets += entry.Title.Text;
+			}
+			return View ();
+		}
     }
 }
