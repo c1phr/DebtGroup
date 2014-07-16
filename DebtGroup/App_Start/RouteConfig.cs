40de13066810041c0b214903e1e154ab88ee3256
Ryan Batchelder <rb688@nau.edu>
2014-07-16 08:57:34 -0700

    Route ordering modification

--- DebtGroup/App_Start/RouteConfig.cs
+++ DebtGroup/App_Start/RouteConfig.cs
@@ -12,17 +12,16 @@ namespace DebtGroup
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
 			routes.MapRoute (
 				name: "oauth2callback",
 				url: "oauth2callback",
 				defaults: new { controller = "Home", action = "oauth2callback" }
 			);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
