9c068e4756c1a9e08d4a1baf750d0aab8a579295
Ryan Batchelder <rb688@nau.edu>
2014-07-22 08:51:19 -0700

    Landing page is flex-boxed and responsive App page was dumped into the same template, awaiting its own layout

--- DebtGroup/Controllers/HomeController.cs
+++ DebtGroup/Controllers/HomeController.cs
@@ -12,22 +12,16 @@ namespace DebtGroup.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
--- DebtGroup/Extensions/HtmlExtensions.cs
+++ DebtGroup/Extensions/HtmlExtensions.cs
@@ -28,6 +28,26 @@ namespace DebtGroup.HtmlExtensions
 
 			return form.ToString(TagRenderMode.Normal);
 		}
+
+		public static string ActionButton(this HtmlHelper helper, string value, 
+			string action, string controller, object routeValues, string additionalClasses)
+		{
+			var a = (new UrlHelper(helper.ViewContext.RequestContext))
+				.Action(action, controller, routeValues);
+
+			var form = new TagBuilder("form");
+			form.Attributes.Add("method", "get");
+			form.Attributes.Add("action", a);
+
+			var input = new TagBuilder("input");
+			input.Attributes.Add("type", "submit");
+			input.Attributes.Add("value", value);
+			input.Attributes.Add ("class", "btn btn-primary " + additionalClasses);
+
+			form.InnerHtml = input.ToString(TagRenderMode.SelfClosing);
+
+			return form.ToString(TagRenderMode.Normal);
+		}
 	}
 }
 
