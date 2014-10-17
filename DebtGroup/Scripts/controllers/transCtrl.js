b3ce95fe8b8ef132992461bc7a3cc88f5b4b128c
dukeayers <dfa24@nau.edu>
2014-10-17 09:30:46 -0700

    Details is now a modal
    Made the details section refer to a modal.

--- DebtGroup/Scripts/controllers/transCtrl.js
+++ DebtGroup/Scripts/controllers/transCtrl.js
@@ -1,9 +1,26 @@
 ﻿
 
 app.controller('transCtrl', [
-    '$scope', function($scope) {
+    '$scope', '$http', function($scope, $http) {
 
-        
+        $scope.currentDetails = [];
+        $scope.detailCounter = 0;
+        $scope.hasDetails = false;
+        $scope.detailsData = function (itemId) {
+            if (!$scope.hasDetails) {
+                $http.get('/transactions/Details/' + itemId).
+                    success(function(data, status, headers, config) {
+                        $scope.currentDetails.push(data);
+                        $scope.newDetails = $scope.currentDetails;
+                    }).
+                    error(function(data, status, headers, config) {
+                    });
+                $scope.newDetails = '';
+                $scope.hasDetails = true;
+            }
+        }
+
+
 
     }
 ]);
